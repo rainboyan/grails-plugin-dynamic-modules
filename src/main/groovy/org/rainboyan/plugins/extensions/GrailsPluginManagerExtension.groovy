03c0d589021f0dd2a8961f2ebaafe1618d673cb2
Michael Yan <rain@rainboyan.com>
2023-03-10 00:45:05 +0800

    Rename package: org.rainboyan.plugins.extensions

--- src/main/groovy/org/rainboyan/plugins/extensions/GrailsPluginManagerExtension.groovy
+++ src/main/groovy/org/rainboyan/plugins/extensions/GrailsPluginManagerExtension.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.rainboyan.plugins.support
+package org.rainboyan.plugins.extensions
 
 import groovy.transform.CompileStatic
 
